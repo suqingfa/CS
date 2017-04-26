60b7d1bd370982d3be11b8fab801d4eb867922fe
多才 <enmoton@vip.qq.com>
2017-04-26 20:16:08 +0800

    EF Psql

---
+++ EF_Pgsql/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Pgsql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestDBContext context = new TestDBContext();
+            context.Database.Log = Console.WriteLine;
+            context.Database.CreateIfNotExists();
+
+            var a = context.Users.ToArray();
+            Console.WriteLine(a);
+        }
+    }
+
+    public class TestDBContext : DbContext
+    {
+        public TestDBContext() : base("name=TestDBContext") { }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+
+    public class User
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
